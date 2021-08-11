dd161585dfc3645d08d58a06e93d6086aef7d07a
Deeksha Tiwari <deeksha2021@gmail.com>
2021-08-11 20:07:35 +0530

    error handler added

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ var input = document.querySelector("#input-english");
 var btnTranslate = document.querySelector("#btn-translate");
 var output = document.querySelector("#output");
 
+var minionServerURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslateUrl(text) {
+  return minionServerURL + "?" + "text" + text;
+}
+
+function errorHandler(error) {
+  console.log("error occured !!", error);
+  alert("Something went wrong with the server, Try again Later!!");
+}
+
 function clickEventHandler() {
   output.innerText = "chddhh hdjhd hdjhdd " + input.value;
 }
